5fffa43ad4d208787ed4fb4d4113b7a4fd330d42
Muralha <alexmuralhafernandes@gmail.com>
2022-01-25 19:10:01 -0300

    code cleaning, tab adjustments

--- index.js
+++ index.js
@@ -1,28 +1,27 @@
-var img = new Image()
-
 // CANVAS
-canvas1 = document.getElementById('resized')
+canvas1 = document.getElementById('resized-canvas')
 ctx = canvas1.getContext('2d');
 
-
+var img = new Image()
 const photoFile = document.getElementById('photo-file');
-let file_name = document.getElementById('file_name');
+let fileName = document.getElementById('file-name');
+
 
 // SLIDER 1 VARS
-let slider1 = document.getElementById("slider1");
-let slider_value_label = document.getElementById('slider_value_label');
-let output_label = document.getElementById('output_label');
+let sliderSize = document.getElementById("slider-size");
+let sliderValueLabel = document.getElementById('slider-value-label');
+//let outputLabel = document.getElementById('output-label');
 
 
 // QUALITY SLIDER VARS
-let slider_quality = document.getElementById("slider_quality");
-let slider_quality_label = document.getElementById('slider_quality_label');
+let sliderQuality = document.getElementById("slider-quality");
+let sliderQualityLabel = document.getElementById('slider-quality-label');
 
+
+// READ FILE
 document.getElementById('select-image').onclick = function () {
   photoFile.click()
 }
-
-
 
 window.addEventListener('DOMContentLoaded', () => {
   photoFile.addEventListener('change', () => {
@@ -32,45 +31,40 @@ window.addEventListener('DOMContentLoaded', () => {
       img.src = reader.result;
     };
     reader.readAsDataURL(file)
-    
-    file_name.innerHTML = file.name
-    //returns the file name to use in the download button
+    fileName.innerHTML = file.name
+    //returns the file name to use in the download path:
     return file_name_for_download = file.name
   });
 
 })
 
 img.onload = () => {
-  let width = Math.floor(img.naturalWidth * 0.5)
-  let height = Math.floor(img.naturalHeight * 0.5)
+  let width = Math.floor(img.naturalWidth )
+  let height = Math.floor(img.naturalHeight )
   canvas1.width = width;
   canvas1.height = height;
   //ctx.filter = "grayscale(100%)";
   ctx.drawImage(img, 0, 0 , width, height);
-  
-  slider_value_label.innerHTML = slider1.value + "% of original size - " + Math.floor(img.naturalWidth * (slider1.value/100)) + " x " + Math.floor(img.naturalHeight * (slider1.value/100)) + " px"
-  
+  sliderValueLabel.innerHTML = sliderSize.value + "% of original size - " + Math.floor(img.naturalWidth * (sliderSize.value/100)) + " x " + Math.floor(img.naturalHeight * (sliderSize.value/100)) + " px"  
 };
 
+
 // SHOW SLIDER VALUES ON CHANGE
-slider1.onchange = function () {
-  slider_value_label.innerHTML = slider1.value + "% of original size - " + Math.floor(img.naturalWidth * (slider1.value/100)) + " x " + Math.floor(img.naturalHeight * (slider1.value/100)) + " px"
-  //output_label.innerHTML = slider1.value + "% of original size - " + Math.floor(img.naturalWidth * (slider1.value/100)) + " x " + Math.floor(img.naturalHeight * (slider1.value/100)) + " px"
+sliderSize.onchange = function () {
+  sliderValueLabel.innerHTML = sliderSize.value + "% of original size - " + Math.floor(img.naturalWidth * (sliderSize.value/100)) + " x " + Math.floor(img.naturalHeight * (sliderSize.value/100)) + " px"
+  //outputLabel.innerHTML = sliderSize.value + "% of original size - " + Math.floor(img.naturalWidth * (sliderSize.value/100)) + " x " + Math.floor(img.naturalHeight * (sliderSize.value/100)) + " px"
 }
-
-slider_quality.onchange = function () {
-  slider_quality_label.innerHTML = "Quality: " + slider_quality.value
+sliderQuality.onchange = function () {
+  sliderQualityLabel.innerHTML = "Quality: " + sliderQuality.value
 }
-
 
 
 // DOWNLOAD BUTTON
+const downloadButton = document.getElementById('download-button');
 
-const download_button = document.getElementById('download_button');
-
-download_button.onclick = function () { 
-  let width = Math.floor(img.naturalWidth * (slider1.value/100))
-  let height = Math.floor(img.naturalHeight * (slider1.value/100))
+downloadButton.onclick = function () { 
+  let width = Math.floor(img.naturalWidth * (sliderSize.value/100))
+  let height = Math.floor(img.naturalHeight * (sliderSize.value/100))
   canvas1.width = width;
   canvas1.height = height;
   //ctx.filter = "grayscale(100%)";
@@ -78,11 +72,7 @@ download_button.onclick = function () {
   ctx.drawImage(img, 0, 0 , width, height);
   const a = document.createElement('a');
   a.download = file_name_for_download.substring(0, file_name_for_download.length - 4) + '_resized.jpg';
-  a.href = canvas1.toDataURL('image/jpeg', slider_quality.value/10);
+  a.href = canvas1.toDataURL('image/jpeg', sliderQuality.value/10);
   a.click();
   console.log(file_name_for_download)
 }
-
-
-
-

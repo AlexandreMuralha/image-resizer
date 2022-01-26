56e9762ad527b247a50b3be91096e5a72abce49a
Muralha <alexmuralhafernandes@gmail.com>
2022-01-25 22:38:06 -0300

    code cleaning, tab adjustments 2

--- index.js
+++ index.js
@@ -6,17 +6,14 @@ var img = new Image()
 const photoFile = document.getElementById('photo-file');
 let fileName = document.getElementById('file-name');
 
-
 // SLIDER 1 VARS
 let sliderSize = document.getElementById("slider-size");
 let sliderValueLabel = document.getElementById('slider-value-label');
 //let outputLabel = document.getElementById('output-label');
 
-
 // QUALITY SLIDER VARS
 let sliderQuality = document.getElementById("slider-quality");
 let sliderQualityLabel = document.getElementById('slider-quality-label');
-
 
 // READ FILE
 document.getElementById('select-image').onclick = function () {
@@ -48,7 +45,6 @@ img.onload = () => {
   sliderValueLabel.innerHTML = sliderSize.value + "% of original size - " + Math.floor(img.naturalWidth * (sliderSize.value/100)) + " x " + Math.floor(img.naturalHeight * (sliderSize.value/100)) + " px"  
 };
 
-
 // SHOW SLIDER VALUES ON CHANGE
 sliderSize.onchange = function () {
   sliderValueLabel.innerHTML = sliderSize.value + "% of original size - " + Math.floor(img.naturalWidth * (sliderSize.value/100)) + " x " + Math.floor(img.naturalHeight * (sliderSize.value/100)) + " px"
@@ -57,7 +53,6 @@ sliderSize.onchange = function () {
 sliderQuality.onchange = function () {
   sliderQualityLabel.innerHTML = "Quality: " + sliderQuality.value
 }
-
 
 // DOWNLOAD BUTTON
 const downloadButton = document.getElementById('download-button');

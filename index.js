38576dd607e8e39b8a058eef39f26003d036eabd
Muralha <alexmuralhafernandes@gmail.com>
2022-02-18 15:06:40 -0300

    added drag and drop features; one collumn design

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ canvas1 = document.getElementById('resized-canvas')
 ctx = canvas1.getContext('2d');
 
 var img = new Image()
-const photoFile = document.getElementById('photo-file');
+const photoFile = document.getElementById('photo-file-input');
 let fileName = document.getElementById('file-name');
 
 // SLIDER 1 VARS
@@ -43,6 +43,10 @@ img.onload = () => {
   //ctx.filter = "grayscale(100%)";
   ctx.drawImage(img, 0, 0 , width, height);
   sliderValueLabel.innerHTML = sliderSize.value + "% of original size - " + Math.floor(img.naturalWidth * (sliderSize.value/100)) + " x " + Math.floor(img.naturalHeight * (sliderSize.value/100)) + " px"  
+  document.getElementById('select-image-div').style.display = "none";
+  document.getElementById('select-image').innerHTML = "Browse another image ↑";
+  document.getElementById('download-div').style.display = "flex";
+  document.getElementById('image-div').appendChild(document.getElementById('select-image'));
 };
 
 // SHOW SLIDER VALUES ON CHANGE
@@ -71,3 +75,40 @@ downloadButton.onclick = function () {
   a.click();
   console.log(file_name_for_download)
 }
+
+
+// DRAG AND DROP
+
+const dropArea = document.getElementById('drag-area');
+
+
+//Drag File Over DragArea
+dropArea.addEventListener("dragover", ()=>{
+  event.preventDefault();
+  console.log("file is DragArea")
+  dropArea.classList.add('active');
+})
+
+//File Leave DragArea
+dropArea.addEventListener("dragleave", ()=>{
+  console.log("file is out DragArea")
+  dropArea.classList.remove('active');
+})
+
+//File Dropped on DragArea
+dropArea.addEventListener("drop", (event)=>{
+  event.preventDefault(); //
+  console.log("file is dropped on DragArea")
+
+  file = event.dataTransfer.files[0];
+  const reader = new FileReader();
+  reader.onload = () => {
+    img.src = reader.result;
+  };
+  reader.readAsDataURL(file)
+  fileName.innerHTML = file.name
+  //returns the file name to use in the download path:
+  return file_name_for_download = file.name
+
+
+})
